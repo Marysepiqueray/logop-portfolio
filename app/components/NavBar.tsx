0599c457e39489814f4e8e7acc8961495418e778
Marysepiqueray <marysepiqueray@gmail.com>
2026-04-24 09:33:17 +0200

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,12 +4,10 @@ import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 
 export default function NavBar() {
-
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     async function checkRole() {
-
       const { data: userData } = await supabase.auth.getUser();
       const userId = userData.user?.id;
 
@@ -24,21 +22,27 @@ export default function NavBar() {
       if (membre?.role === "admin") {
         setIsAdmin(true);
       }
-
     }
 
     checkRole();
   }, []);
 
   return (
-
     <nav className="navbar">
-  <a href="/">Accueil</a>
-  <a href="/me">Mon portfolio</a>
-  <a href="/annuaire">Annuaire</a>
-  <a href="/carte">Carte</a>
-  {isAdmin && <a href="/admin">Admin</a>}
-</nav>
+      <a href="/">Accueil</a>
+      <a href="/me">Mon portfolio</a>
+      <a href="/annuaire">Annuaire</a>
+      <a href="/carte">Carte</a>
 
+      <a
+        href="https://www.logopaidetvous.be/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Site ASBL
+      </a>
+
+      {isAdmin && <a href="/admin">Admin</a>}
+    </nav>
   );
 }
